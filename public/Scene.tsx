cef4a335ff52cf7b0653cc60a0f80d90b8256857
newFakeF0rest <azizmamadjaprio@gmail.com>
2024-06-26 22:22:35 +0500

    testing new features of react three fiber

--- public/Scene.tsx
+++ public/Scene.tsx
@@ -8,48 +8,166 @@ Title: Hangers W Clothes
 */
 
 import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react'
 import * as THREE from 'three'
 import { GLTF } from 'three-stdlib'
 
 type GLTFResult = GLTF & {
-  nodes: {
-    Object_2: THREE.Mesh;
-    Object_3: THREE.Mesh;
-    Object_4: THREE.Mesh;
-    Object_5: THREE.Mesh;
-  };
-  materials: {
-    Pocket_FRONT_7674: THREE.MeshStandardMaterial;
-    Material7471: THREE.MeshStandardMaterial;
-    Tshirt_FRONT_7678: THREE.MeshStandardMaterial;
-    Waist_Belt_FRONT_7670: THREE.MeshStandardMaterial;
-  };
+	nodes: {
+		Object_2: THREE.Mesh;
+		Object_3: THREE.Mesh;
+		Object_4: THREE.Mesh;
+		Object_5: THREE.Mesh;
+		Object_6: THREE.Mesh;
+		Object_7: THREE.Mesh;
+		Object_8: THREE.Mesh;
+		Object_9: THREE.Mesh;
+		Object_10: THREE.Mesh;
+		Object_11: THREE.Mesh;
+		Object_12: THREE.Mesh;
+		Object_13: THREE.Mesh;
+		Object_14: THREE.Mesh;
+		Object_15: THREE.Mesh;
+		Object_16: THREE.Mesh;
+		Object_17: THREE.Mesh;
+		Object_18: THREE.Mesh;
+		Object_19: THREE.Mesh;
+		Object_20: THREE.Mesh;
+		Object_21: THREE.Mesh;
+		Object_22: THREE.Mesh;
+		Object_23: THREE.Mesh;
+		Object_24: THREE.Mesh;
+		Object_25: THREE.Mesh;
+		Object_26: THREE.Mesh;
+		Object_27: THREE.Mesh;
+	};
+	materials: {
+		Badges1Mtl: THREE.MeshStandardMaterial;
+		Banner1Mtl: THREE.MeshStandardMaterial;
+		Glass1Mtl: THREE.MeshStandardMaterial;
+		Cab1Mtl: THREE.MeshStandardMaterial;
+		Chassis1Mtl: THREE.MeshStandardMaterial;
+		Lights1Mtl: THREE.MeshStandardMaterial;
+		Bodywork1Mtl: THREE.MeshStandardMaterial;
+		BodyworkL21Mtl: THREE.MeshStandardMaterial;
+		Cupper1Mtl: THREE.MeshStandardMaterial;
+		Disc1Mtl: THREE.MeshStandardMaterial;
+		Caliper1Mtl: THREE.MeshStandardMaterial;
+		Hub1Mtl: THREE.MeshStandardMaterial;
+		Meshpart2Mtl: THREE.MeshStandardMaterial;
+		Meshpart3Mtl: THREE.MeshStandardMaterial;
+		Meshpart1Mtl: THREE.MeshStandardMaterial;
+		Misc1Mtl: THREE.MeshStandardMaterial;
+		Meshpart4Mtl: THREE.MeshStandardMaterial;
+		Springtop1Mtl: THREE.MeshStandardMaterial;
+		Upperarm1Mtl: THREE.MeshStandardMaterial;
+		Upperarmb1Mtl: THREE.MeshStandardMaterial;
+	};
 };
 
-export function Model(props: JSX.IntrinsicElements["group"]) {
-  const { nodes, materials } = useGLTF("/scene.gltf") as GLTFResult;
-  return (
-    <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
-        <mesh
-          geometry={nodes.Object_2.geometry}
-          material={materials.Pocket_FRONT_7674}
-        />
-        <mesh
-          geometry={nodes.Object_3.geometry}
-          material={materials.Material7471}
-        />
-        <mesh
-          geometry={nodes.Object_4.geometry}
-          material={materials.Tshirt_FRONT_7678}
-        />
-        <mesh
-          geometry={nodes.Object_5.geometry}
-          material={materials.Waist_Belt_FRONT_7670}
-        />
-      </group>
-    </group>
-  );
+export function Scene(props: JSX.IntrinsicElements['group']) {
+	const { nodes, materials } = useGLTF('/scene.gltf') as GLTFResult
+	const groupRef = useRef<THREE.Group>(null)
+	
+	// useFrame((_state, delta) => {
+	// 	if (groupRef.current) {
+	// 		// groupRef.current.rotation.z += delta * 0.0025 // Вращаем модель вокруг оси Y
+	// 		// groupRef.current.rotation.x += delta * 0.0025
+	// 		groupRef.current.rotation.y += delta * 0.0025
+	// 	}
+	// })
+	
+	return (
+		<group ref={groupRef}  {...props} dispose={null}>
+			<group rotation={[-Math.PI / 2, 0, 0]}>
+				<mesh castShadow receiveShadow geometry={nodes.Object_2.geometry} material={materials.Badges1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_3.geometry} material={materials.Banner1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_4.geometry} material={materials.Glass1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_5.geometry} material={materials.Cab1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_6.geometry} material={materials.Chassis1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_7.geometry} material={materials.Lights1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_8.geometry} material={materials.Bodywork1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_9.geometry} material={materials.Bodywork1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_10.geometry} material={materials.BodyworkL21Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_11.geometry} material={materials.BodyworkL21Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_12.geometry} material={materials.Cupper1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_13.geometry} material={materials.Disc1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_14.geometry} material={materials.Caliper1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_15.geometry} material={materials.Hub1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_16.geometry} material={materials.Meshpart2Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_17.geometry} material={materials.Meshpart2Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_18.geometry} material={materials.Meshpart2Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_19.geometry} material={materials.Meshpart2Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_20.geometry} material={materials.Meshpart3Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_21.geometry} material={materials.Meshpart1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_22.geometry} material={materials.Misc1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_23.geometry} material={materials.Misc1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_24.geometry} material={materials.Meshpart4Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_25.geometry} material={materials.Springtop1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_26.geometry} material={materials.Upperarm1Mtl} />
+				<mesh castShadow receiveShadow geometry={nodes.Object_27.geometry} material={materials.Upperarmb1Mtl} />
+			</group>
+		</group>
+	)
 }
 
-useGLTF.preload("/scene.gltf");
+useGLTF.preload('/scene.gltf')
+
+
+// import { useGLTF } from '@react-three/drei'
+// import { useFrame } from '@react-three/fiber'
+// import { useRef } from 'react'
+// import type { Object3DEventMap } from 'three'
+// import * as THREE from 'three'
+// import { GLTF } from 'three-stdlib'
+//
+// type GLTFResult = GLTF & {
+//   nodes: {
+//     Object_2: THREE.Mesh;
+//     Object_3: THREE.Mesh;
+//     Object_4: THREE.Mesh;
+//     Object_5: THREE.Mesh;
+//   };
+//   materials: {
+//     Pocket_FRONT_7674: THREE.MeshStandardMaterial;
+//     Material7471: THREE.MeshStandardMaterial;
+//     Tshirt_FRONT_7678: THREE.MeshStandardMaterial;
+//     Waist_Belt_FRONT_7670: THREE.MeshStandardMaterial;
+//   };
+// };
+//
+// export function Model(props: JSX.IntrinsicElements["group"]) {
+//   const meshRef = useRef<THREE.Group<Object3DEventMap>>(null);
+//   const { nodes, materials } = useGLTF("/scene.gltf") as GLTFResult;
+//
+//   useFrame((_state, delta) => {
+//     if (meshRef.current) {
+//       meshRef.current.position.y += delta;
+//     }
+//   });
+//
+//   return (
+//     <group {...props} dispose={null}>
+//       <group ref={meshRef} rotation={[-Math.PI / 2, 0, 0]}>
+//         <mesh
+//           geometry={nodes.Object_2.geometry}
+//           material={materials.Pocket_FRONT_7674}
+//         />
+//         <mesh
+//           geometry={nodes.Object_3.geometry}
+//           material={materials.Material7471}
+//         />
+//         <mesh
+//           geometry={nodes.Object_4.geometry}
+//           material={materials.Tshirt_FRONT_7678}
+//         />
+//         <mesh
+//           geometry={nodes.Object_5.geometry}
+//           material={materials.Waist_Belt_FRONT_7670}
+//         />
+//       </group>
+//     </group>
+//   );
+// }
+//
+// useGLTF.preload("/scene.gltf");
--- src/Test.tsx
+++ src/Test.tsx
@@ -1,5 +1,7 @@
-import { PresentationControls, Stage, useGLTF } from "@react-three/drei";
+import { OrbitControls, Stage, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Scene } from "../public/Scene.tsx";
 
 export const Model = () => {
   const { scene } = useGLTF("/scene.gltf");
@@ -8,15 +10,21 @@ export const Model = () => {
 
 export const Test = () => {
   return (
-    <Canvas>
-      {/* <color attach={"background"} args={["#101010"]}></color> */}
-      <PresentationControls>
-        <Stage environment={"sunset"}>
-          <directionalLight intensity={1} position={[0, 3, 2]} />
-          <Model></Model>
-          {/* <Scene /> */}
+    <Canvas shadows={false} dpr={[1, 2]}>
+      <Suspense fallback={null}>
+        {/* <color attach={"background"} args={["#101010"]}></color> */}
+        {/* <PresentationControls> */}
+        {/*   <Stage environment={"sunset"}> */}
+        {/*     <directionalLight intensity={1} position={[0, 3, 2]} /> */}
+        {/*     /!* <Model></Model> *!/ */}
+        {/*   </Stage> */}
+        {/* </PresentationControls> */}
+
+        <Stage preset="rembrandt" intensity={1} environment="city">
+          <Scene />
         </Stage>
-      </PresentationControls>
+      </Suspense>
+      <OrbitControls autoRotate />
     </Canvas>
   );
 };
